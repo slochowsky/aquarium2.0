16d599aeb7ef000d89c8db8b03d9e5cb87bb7933
kevinpenny3 <kevin.penny3@yahoo.com>
2019-11-22 10:53:06 -0600

    Updated fish collection and objects

---
+++ martins_aquarium/scripts/fishDataProvider.js
@@ -0,0 +1,58 @@
+const fishCollection = [
+    {
+        name: "Bart",
+        species: "Butterfly Fish",
+        food: ["algae", "crustaceans"],
+        length: 4,
+        location: "Great Barrier Reef",
+        image: "goldie-fish.jpg",
+    },
+
+    {
+        name: "Walter",
+        species: "Whale",
+        location: "Pacific Ocean",
+        length: 900,
+        food: ["Fish"],
+        image: "goldie-fish.jpg",
+    },
+
+    {
+        name: "Dragon",
+        species: "Firemouth Chiclid",
+        location: "Petsmart",
+        length: 6,
+        food: ["Flakes", "Pellets"],
+        image: "goldie-fish.jpg",
+    },
+
+    {
+        name: "Thiccums",
+        species: "Puffer Fish",
+        location: "Subtropical Waters",
+        length: 36,
+        food: ["Clams", "Shellfish"],
+        image: "goldie-fish.jpg",
+    },
+    {
+        name: "Zappy Boy",
+        species: "Electric Catfish",
+        location: "Power Plant puddles",
+        length: 6,
+        food: ["lightning"],
+        image: "goldie-fish.jpg",
+    },
+    {
+        name: "Long Face",
+        species: "Elephant Nose",
+        location: "Sewers",
+        length: 4,
+        food: ["algae", "Shellfish"],
+        image: "goldie-fish.jpg",
+    }
+]
+
+// This is new code. Add this.
+export const useFish = () => {
+    return fishCollection
+}
--- martins_aquarium/scripts/main.js
+++ martins_aquarium/scripts/main.js
@@ -5,7 +5,15 @@
 import initializeDetailButtonEvents from './dialogs.js'
 import addMouseOverEventListeners from './highlights.js'
 import addTipMouseOverEventListeners from './borders.js'
+import { useFish } from './fishDataProvider.js'
 
 initializeDetailButtonEvents()
 addMouseOverEventListeners()
 addTipMouseOverEventListeners()
+const allOfTheFish = useFish()
+
+console.log("all of the fish:", allOfTheFish)
+
+for (const fish of allOfTheFish) {
+    console.log(fish)
+}
